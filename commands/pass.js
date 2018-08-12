2abdb8bd58995cca676103d5ff28ec2b99b19540
Nathan N <Nitharios@users.noreply.github.com>
2018-08-11 14:12:52 -1000

    Added more pass logic

--- commands/pass.js
+++ commands/pass.js
@@ -3,19 +3,22 @@ module.exports = {
   description: 'pass to the next user',
   args: true,
   execute(message, args) {
-    const reply = message.channel;
+    const reply = message.channel;    
     const target = message.channel.members.find(member => {
       return member.user.username === args[0];
     });
 
     if (!target) {
-      return reply.send(`Oh no! That person doesn't exist :(`);
+      return reply.send(`Oh no, ${ message.author }! That person doesn't exist :(`);
     
-    } else if (target === message.author) {
-      return reply.send(`You can't pass to yourself!`);
+    } else if (target.user.username === message.author.username) {
+      return reply.send(`You can't pass to yourself, ${ message.author }!`);
+
+    } else if (target.bot) {
+      return reply.send(`You can't pass to a bot, ${ message.author }!`);
 
     } else {
-      return reply.send(`${message.author} passes to ${target}`);
+      return reply.send(`${ message.author } passes to ${ target }`);
     
     }
   }
