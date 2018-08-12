87c486568ec601e604a01f12a1afc5c5c137476a
Nathan N <Nitharios@users.noreply.github.com>
2018-08-11 15:01:25 -1000

    Updated help library

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 module.exports = {
-  name: 'ignite',
+  name: 'help',
   description: 'List of Hot Botato commands and their descriptions.',
   args: false,
   execute(message, args) {
-    message.channel.send(`\`List of commands: \r\n\
-1. =ignite: Ignites the botato!\r\n
-2. =pass [user]: Pass the botato to a specified user.
-3. =help: Information about Hot Botato.\``);
+    message.channel.send(`\`\`\`List of commands:
+
+1. ignite: Ignites the botato!
+2. pass [user]: Pass the botato to a specified user.
+3. help: Information about Hot Botato.\`\`\``);
   }
 }
